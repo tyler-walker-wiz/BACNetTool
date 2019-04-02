ec69095bb47c02d62ea546ee690f3b8abf063431
Tyler Walker <tyler@prenticeworx.com>
2019-04-01 18:40:00 -0600

    Add project files.

---
+++ BacNetTool/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BacNetTool.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
